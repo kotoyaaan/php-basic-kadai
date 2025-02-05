7c888cd2ccbcb2c246ca2c6548f4b6b292d2fbef
kotoyaaan <narumimurakami0115@gmail.com>
2025-02-05 23:48:18 +0900

    課題をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>連想配列を作ってキーと値を出力しよう</title>
+</head>
+<body>
+  <p>
+    <?php
+    $spec = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+    print_r($spec);
+    ?>
+</body>
+</html>
